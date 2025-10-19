36836aad1fd6eca5bd610e8cacce02633e38fdb0
Seyyed Nima Rounaghi <nimarounaghi11@gmail.com>
2025-10-19 20:17:57 +0330

    add constructor and class Node & Linked List base code

--- Linked List/Linked List.cpp
+++ Linked List/Linked List.cpp
@@ -1,6 +1,49 @@
 ﻿#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int data) {
+        this->data = data;
+        next = NULL;
+    }
+
+};
+
+
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+    int length;
+
+public:
+    LinkedList(int data) {
+        Node* NewNode = new Node(data);
+        head = NewNode;
+        tail = NewNode;
+        length = 1;
+    }
+    void display() {
+        Node* ptr = head;
+        while (ptr != NULL)
+        {
+            cout << ptr->data << endl;
+            ptr = ptr->next;
+        }
+        cout << endl;
+    }
+};
+
+
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    LinkedList l1(4);
+    l1.display();
 }
