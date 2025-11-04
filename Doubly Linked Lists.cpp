e88369430ac4078139bb825c28a851aa60b10fb2
Seyyed Nima Rounaghi <nimarounaghi11@gmail.com>
2025-11-04 22:27:19 +0330

    Add class Node and mian class + display function

--- Doubly Linked Lists.cpp
+++ Doubly Linked Lists.cpp
@@ -1,8 +1,47 @@
 ﻿#include <iostream>
 using namespace std;
 
+class Node {
+public:
+    int value;
+    Node* next;
+    Node* pre;
+
+    Node(int value) {
+        this->value = value;
+        this->next = NULL;
+        this->pre = NULL;
+    }
+};
+
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+    int length;
+public:
+    DoublyLinkedList(int value) {
+        Node* newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    void display() {
+        Node* tmp = head;
+        while (tmp)
+        {
+            cout << tmp->value << endl;
+            tmp = tmp->next;
+        }
+
+    }
+};
+
 
 int main()
 {
-    
+    DoublyLinkedList* myl1 = new DoublyLinkedList(7);
+    myl1->display();
 }
