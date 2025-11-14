e1a9ada8919649be9a92da4b9b7331661916d69c
Seyyed Nima Rounaghi <nimarounaghi11@gmail.com>
2025-11-14 18:20:05 +0330

    Add deleteLast function

--- Doubly Linked Lists.cpp
+++ Doubly Linked Lists.cpp
@@ -50,6 +50,22 @@ public:
         }
         length++;
     }
+    void deleteLast() {
+        if (length == 0) {
+            return;
+        }
+        Node* tmp = tail;
+        if (length == 1) {
+            tail = NULL;
+            head = NULL;
+        }
+        else {
+            tail = tail->pre;
+            tail->next = NULL;
+        }
+        delete tmp;
+        length--;
+    }
 };
 
 
@@ -57,5 +73,6 @@ int main()
 {
     DoublyLinkedList* myl1 = new DoublyLinkedList(7);
     myl1->append(2);
+    myl1->deleteLast();
     myl1->display();
 }
