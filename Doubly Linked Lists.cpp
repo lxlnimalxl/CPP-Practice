3903f8ff7cd1007c5ace5f9785b547f2c7746947
Seyyed Nima Rounaghi <nimarounaghi11@gmail.com>
2025-11-14 18:03:51 +0330

    Add append function

--- Doubly Linked Lists.cpp
+++ Doubly Linked Lists.cpp
@@ -37,11 +37,25 @@ public:
         }
 
     }
+    void append(int value) {
+        Node* newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            newNode->pre = tail;
+            tail = newNode;
+        }
+        length++;
+    }
 };
 
 
 int main()
 {
     DoublyLinkedList* myl1 = new DoublyLinkedList(7);
+    myl1->append(2);
     myl1->display();
 }
