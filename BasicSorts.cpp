ffc3615d8edaa2b35b30cf03c80a4331e13d2aab
Seyyed Nima Rounaghi <nimarounaghi11@gmail.com>
2025-12-31 19:02:29 +0330

    add basicSort in Cpp

--- BasicSorts.cpp
+++ BasicSorts.cpp
@@ -33,7 +33,17 @@ void SelectionSort(int arr[] , int size){
 }
 
 
-
+void InsertionSort(int arr[] , int size){
+    for(int i = 1; i<size; i++){
+        int tmp = arr[i];
+        int j = i - 1;
+        while(j > -1 && tmp < arr[j]){
+            arr[j+1] = arr[j];
+            arr[j] = tmp;
+            j--;
+        }
+    }
+}
 
 
 
